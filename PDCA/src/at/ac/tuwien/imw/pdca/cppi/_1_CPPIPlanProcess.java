9a4ec8ec61c665e3a20011620af13a39bd92cd38
Mateusz Czernecki <mathck@gmail.com>
2016-02-01 18:23:42 +0100

    added more doc, removed bin folder

--- PDCA/src/at/ac/tuwien/imw/pdca/cppi/_1_CPPIPlanProcess.java
+++ PDCA/src/at/ac/tuwien/imw/pdca/cppi/_1_CPPIPlanProcess.java
@@ -3,10 +3,15 @@ package at.ac.tuwien.imw.pdca.cppi;
 import at.ac.tuwien.imw.pdca.PlanProcess;
 
 /**
- * 1. Plan = 1abcdefg
- * 2. Do = 2, 2a
- * 3. Check = 3, 3a
- * 4. Act = 4, 4a, 4b
+ * @author mathc_000
+ * 
+ * Plan, Do, Check, Act
+ * https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/PDCA_Cycle.svg/2000px-PDCA_Cycle.svg.png
+ * 
+ * 1. Plan = 1abcdefg (use PlanProcess, PlanningRules, PlanConfiguration)
+ * 2. Do = 2, 2a (use DoRules, DoProcess)
+ * 3. Check = 3, 3a (use CheckingRules, CheckProcess)
+ * 4. Act = 4, 4a, 4b (use ActProcess, CorrectiveActOutput??, CorrectiveActRules??)
  */
 
 /**
--- PDCA/src/at/ac/tuwien/imw/pdca/cppi/service/CPPISimulation.java
+++ PDCA/src/at/ac/tuwien/imw/pdca/cppi/service/CPPISimulation.java
@@ -35,6 +35,18 @@ public class CPPISimulation {
 		
 		//...
 		
+		// HOW TO DO Synchronization IN JAVA : TUTORIAL ->
+		// http://www.fh-wedel.de/~si/vorlesungen/java/OOPMitJava/Multithreading/Synchronisation.html
+		
 		new Thread(new CPPIStockPriceGenerator()).start();
+		
+		/**
+		 * General Idea:
+		 * 
+		 * start Plan	(which runs 1, 1a, 1b, 1c, 1d, 1e, 1f, 1g)
+		 * start Do		(which runs 2, 2a)
+		 * start Check	(which runs 3, 3a)
+		 * start Act	(which runs 4, 4a, 4b)
+		 */
 	}
 }
